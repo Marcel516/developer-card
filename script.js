7c3374756e9260779871bfe9ca8c298ff1f39352
Marcel Kaufmann <marcel.kaufmann_9.7@web.de>
2026-08-09 22:21:45 +0200

    JavaScript Bedingungen und logische Operatoren geübt

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ let messageVisible = false;
 contactButton.addEventListener("click", showContactMessage);
 
 function showContactMessage(){
-    if (messageVisible === false) {
+    if (!messageVisible) {
         contactMessage.textContent = "Du kannst mich über GitHub kontaktieren.";
         contactButton.textContent = "Schließen";
-        messageVisible = true;
     } else {
         contactMessage.textContent = "";
         contactButton.textContent ="Kontakt";
-        messageVisible = false;
     }  
+
+    messageVisible = !messageVisible;
 }
+
