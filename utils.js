b6d070d19d78fea9b8b1b9ddb6d742c30e590546
Marcel Kaufmann <marcel.kaufmann_9.7@web.de>
2026-09-02 22:58:31 +0200

    JavaScript Module mit Import und Export geübt

---
+++ utils.js
@@ -0,0 +1,11 @@
+export function greet(name) {
+    return `Hallo ${name}`;
+}
+
+export function bye(name) {
+    return `Tschüss ${name}`;
+}
+
+export default function multiply(a, b) {
+    return a * b;
+}
